0fb6bfc497c5a3674f3b3571baf80da589f1ec5f
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-06 02:49:24 +0300

    Updated the main_cli

--- main_cli.py
+++ main_cli.py
@@ -1,4 +1,5 @@
 import argparse
+import config
 import todo_bot
 
 
@@ -12,13 +13,17 @@ def start_webhooks(args):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('telegram_api_token')
     parser.set_defaults(func=start_polling)
+    parser.add_argument('-t', '--token', 
+        metavar='TELEGRAM_API_TOKEN', 
+        default=config.TELEGRAM_API_TOKEN, 
+        required=True,
+    )
 
     subparsers  = parser.add_subparsers()
     webhooks_parser = subparsers.add_parser('webhooks', aliases=['wh'], help='use webhooks instead of polling')
     webhooks_parser.add_argument('url')
-    webhooks_parser.add_argument('port')
+    webhooks_parser.add_argument('port', type=int)
     webhooks_parser.set_defaults(func=start_webhooks)
 
     args = parser.parse_args()
